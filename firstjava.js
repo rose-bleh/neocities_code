8a9bd6c7dcc1fca0772a9108db3f54043535e6c8
rose <rosed.20104@gmail.com>
2026-03-31 18:59:03 -0400

    FINALLY FIXED THE PLAYLIST OPTIONS!!
    why did it take me over 3 hours to do this i hate javascript

--- firstjava.js
+++ firstjava.js
@@ -9,6 +9,7 @@ let show_hide = function(btn) {
     }
 }
 //music!!
+const playlistSelect = document.getElementById("playlist_select");
 let coverArt = document.querySelector(".cover_art");
 let songTitle = document.querySelector(".song_name");
 let artistName = document.querySelector(".artist_name");
@@ -26,125 +27,128 @@ let isPlaying = false;
 let updateTimer;
 let currentSong = document.getElementById("music");
 
-let song_list = [
-{
-img: "cologne_beabadoobee.png",
-name:"Cologne",
-artist:"beabadoobee",
-music:"https://files.catbox.moe/m54tmg.mp3"
-},
-{
-img: "takeabite_beabadoobee.png",
-name:"Take A Bite",
-artist:"beabadoobee",
-music:"https://files.catbox.moe/ppp8dt.mp3"
-},
-{
-img: "takeabite_beabadoobee.png",
-name:"Real Man",
-artist:"beabadoobee",
-music:"https://files.catbox.moe/xuig04.mp3"
-},
-{
-img: "amoeba_clairo.png",
-name:"Amoeba",
-artist:"Clairo",
-music:"https://files.catbox.moe/8fvwo0.mp3"
-},
-];
-loadSong(song_index);
- 
-function loadSong(song_index) {
+const playlists = {
+"preformative ahaha": [
+    {img: "cologne_beabadoobee.png",
+    name: "Cologne",
+    artist: "beabadoobee",
+    music: "https://files.catbox.moe/7nz8vt.mp3"},
+    {img: "takeabite_beabadoobee.png",
+    name: "Take A Bite",
+    artist: "beabadoobee",
+    music: "https://files.catbox.moe/ppp8dt.mp3"},
+    {img: "takeabite_beabadoobee.png",
+    name: "Real Man",
+    artist: "beabadoobee",
+    music: "https://files.catbox.moe/xuig04.mp3"},
+    {img: "amoeba_clairo.png",
+    name: "Amoeba",
+    artist: "Clairo",
+    music: "https://files.catbox.moe/8fvwo0.mp3"}
+]
+};
+//dropdown
+for (let key in playlists) {
+    let option = document.createElement("option");
+    option.value = key;
+    option.textContent = key;
+    playlistSelect.appendChild(option);
+}
+
+//change playlist
+playlistSelect.addEventListener("change", (e) => {
+    loadPlaylist(e.target.value);
+});
+
+//load playlist
+function loadPlaylist(name) {
+    currentPlaylist = playlists[name];
+    song_index = 0;
+    loadSong(song_index);
+}
+
+function loadSong(index) {
     clearInterval(updateTimer);
     resetValues();
 
-    // load a new track
-    currentSong.src = song_list[song_index].music;
+    let song = currentPlaylist[index];
+
+    currentSong.src = song.music;
     currentSong.load();
 
-    // update details of the track
-    songTitle.textContent = song_list[song_index].name;
-    artistName.textContent = song_list[song_index].artist;
-    coverArt.style.backgroundImage = "url(" + song_list[song_index].img + ")";
+    songTitle.textContent = song.name;
+    artistName.textContent = song.artist;
+
+    coverArt.style.backgroundImage = "url(" + song.img + ")";
     coverArt.style.backgroundSize = "cover";
 
-    // set an interval of 1000 milliseconds for updating the seek slider
     updateTimer = setInterval(seekUpdate, 1000);
-
-    // move to the next track if the current one finishes playing 
     currentSong.onended = nextSong;
 }
-function resetValues(){
+
+function resetValues() {
     currentTime.textContent = "00:00";
     songLength.textContent = "00:00";
     seekSlider.value = 0;
 }
-// load the first track in the tracklist
-loadSong(song_index);
 
-// checks if song is playing
-function playpauseSong(){
+function playpauseSong() {
     if (!isPlaying) playSong();
     else pauseSong();
 }
 
-// plays track when play button is pressed
-function playSong(){
+function playSong() {
     currentSong.play();
     isPlaying = true;
-
-    // replace icon with the pause icon
-    //playPause.innerHTML = 'pause';
 }
 
-// pauses track when pause button is pressed
-function pauseSong(){
+function pauseSong() {
     currentSong.pause();
     isPlaying = false;
-    //playPause.innerHTML = 'play';
 }
 
-// moves to the next track
-function nextSong(){
-    if (song_index < song_list.length - 1){
-        song_index += 1;
-    }else {
+function nextSong() {
+    if (song_index < currentPlaylist.length - 1) {
+        song_index++;
+    } else {
         song_index = 0;
     }
     loadSong(song_index);
     playSong();
 }
-// moves to the previous track
-function prevSong(){
+
+function prevSong() {
     if (song_index > 0) {
-        song_index -= 1;
+        song_index--;
     } else {
-        song_index = song_list.length-1;
+        song_index = currentPlaylist.length - 1;
     }
     loadSong(song_index);
     playSong();
 }
 
-// seeker slider
-function seekTo(){
+function seekTo() {
     let seekto = currentSong.duration * (seekSlider.value / 100);
     currentSong.currentTime = seekto;
 }
+
 function seekUpdate() {
     if (!isNaN(currentSong.duration)) {
         let seekPosition = currentSong.currentTime * (100 / currentSong.duration);
         seekSlider.value = seekPosition;
 
-        // time calculation
-        let curMins = Math.floor(currentSong.currentTime / 60);
-        let curSecs = Math.floor(currentSong.currentTime - curMins * 60);
-        let durMins = Math.floor(currentSong.duration / 60);
-        let durSecs = Math.floor(currentSong.duration - durMins * 60);
+        let currentMins = Math.floor(currentSong.currentTime / 60);
+        let currentSecs = Math.floor(currentSong.currentTime % 60);
+        let duration_ofMins = Math.floor(currentSong.duration / 60);
+        let duration_ofSecs = Math.floor(currentSong.duration % 60);
 
-        if (curSecs < 10) curSecs = "0" + curSecs;
-        if (durSecs < 10) durSecs = "0" + durSecs;
+        if (currentSecs < 10) currentSecs = "0" + currentSecs;
+        if (duration_ofSecs < 10) duration_ofSecs = "0" + duration_ofSecs;
 
-        currentTime.textContent = curMins + ":" + curSecs;
-        songLength.textContent = durMins + ":" + durSecs;
+        currentTime.textContent = currentMins + ":" + currentSecs;
+        songLength.textContent = duration_ofMins + ":" + duration_ofSecs;
     }
 }
+
+// ===== INIT =====
+loadPlaylist(Object.keys(playlists)[0]);
